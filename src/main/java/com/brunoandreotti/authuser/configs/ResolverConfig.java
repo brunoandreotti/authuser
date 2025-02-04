9a17f986e146c45f8439144aa200bb3a558e4d1d
Bruno <brunoandreotti@outlook.com>
2025-02-04 15:46:56 -0300

    adicionado cors

--- src/main/java/com/brunoandreotti/authuser/configs/ResolverConfig.java
+++ src/main/java/com/brunoandreotti/authuser/configs/ResolverConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,10 @@ public class ResolverConfig implements WebMvcConfigurer {
         resolver.setFallbackPageable(PageRequest.of(0,2));
 
         argumentResolvers.add(resolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").maxAge(3600);
     }
 }
